f270b01f3d3904430e324ceb72d9d7c27e36080d
xan2301 <2silverghost2@gmail.com>
2022-08-12 16:15:16 +0200

    start

---
+++ src/main/java/com/example/tacoclound/TacoCloundApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacoclound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloundApplication.class, args);
+    }
+
+}
